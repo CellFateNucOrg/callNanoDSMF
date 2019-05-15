c905692439db601569fa3fd7e3d5bf889d90f75f
Jennifer Semple <semple.j@gmail.com>
2019-05-15 10:25:14 +0200

    Update basecallGuppy.sh

--- basecallGuppy.sh
+++ basecallGuppy.sh
@@ -2,9 +2,9 @@
 ## script to basecall all fast5 in the folder called fast5files and its recursive directories with Guppy
 ## .bashrc should include location of bin directory inside Guppy directory and export it as ${GUPPYDIR}
 ## uses barcoding
-## output will be in e.g: ../fastqFiles/pass/firstfastq.fastq
-## next the files are sorted by barcode and output to e.g ../bcFastq/pass/barcodeXX.fastq.gz
-## pycoQC is run and results are in ../qc/pycoQC/pycoQC.html
+## output will be in e.g: ./fastqFiles/pass/firstfastq.fastq
+## next the files are sorted by barcode and output to e.g ./bcFastq/pass/barcodeXX.fastq.gz
+## pycoQC is run and results are in ./qc/pycoQC/pycoQC.html
 
 source ./varSettings.sh
 #fullPath=`readlink -f ${relPath}`
