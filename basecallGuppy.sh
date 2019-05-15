9e8c980393b855f84fdd8b12b970d1b56d99688d
Peter Meister <pmeister@binfservms01.unibe.ch>
2019-05-15 10:21:50 +0200

    basecallGuppy.sh filepaths

--- basecallGuppy.sh
+++ basecallGuppy.sh
@@ -1,24 +1,21 @@
 #! /bin/bash
-## script to basecall all fast5 in the folder one level up called fast5files and its recursive directories with Guppy
+## script to basecall all fast5 in the folder called fast5files and its recursive directories with Guppy
 ## .bashrc should include location of bin directory inside Guppy directory and export it as ${GUPPYDIR}
 ## uses barcoding
-## outputs 200,000 reads per fastq file (-q option)
 ## output will be in e.g: ../fastqFiles/pass/firstfastq.fastq
-## next the files are sorted by barcode and output to e.g ../bcFastq/pass/barcodeXX/firstfastq.fastq
+## next the files are sorted by barcode and output to e.g ../bcFastq/pass/barcodeXX.fastq.gz
 ## pycoQC is run and results are in ../qc/pycoQC/pycoQC.html
 
 source ./varSettings.sh
 #fullPath=`readlink -f ${relPath}`
 fullPath=$PWD
-fast5path=$fullPath   #`readlink -f ../`
 
 ##################
 # call barcodes
 #################
 
-#mkdir -p ${fast5path}/bcFast5
-#deepbinner classify --native ${fast5path}/fast5Files > classifications 
-#deepbinner realtime --in_dir ${fast5path}/fast5Files --out_dir ${fast5path}/bcFast5 --native
+mkdir -p ${fullPath}/bcFast5
+deepbinner classify --native ${fullPath}/fast5Files > classifications 
 
 
 ##################
@@ -26,22 +23,21 @@ fast5path=$fullPath   #`readlink -f ../`
 #################
 
 
-#${GUPPYDIR}/guppy_basecaller --input_path ${fast5path}/fast5Files --save_path ${fullPath}/fastqFiles --flowcell FLO-MIN106 --kit SQK-LSK109 --records_per_fastq 200000 --recursive  --cpu_threads_per_caller 8 --qscore_filtering --min_qscore 3 
-#--num_callers
+${GUPPYDIR}/guppy_basecaller --input_path ${fullPath}/fast5Files --save_path ${fullPath}/fastqFiles --flowcell FLO-MIN106 --kit SQK-LSK109 --records_per_fastq 200000 --recursive  --cpu_threads_per_caller 8 --qscore_filtering --min_qscore 3 
 
 
 ##################
 # bin by barcode
 ##################
 
-#mkdir -p ${fullPath}/bcFastq/pass
-#mkdir -p ${fullPath}/bcFastq/fail
+mkdir -p ${fullPath}/bcFastq/pass
+mkdir -p ${fullPath}/bcFastq/fail
 
-#cat ${fullPath}/fastqFiles/pass/* > ${fullPath}/fastqFiles/pass/passed.fq
-#deepbinner bin --classes ${fast5path}/classifications --reads ${fullPath}/fastqFiles/pass/passed.fq --out_dir ${fullPath}/bcFastq/pass
+cat ${fullPath}/fastqFiles/pass/* > ${fullPath}/fastqFiles/pass/passed.fq
+deepbinner bin --classes ${fullPath}/classifications --reads ${fullPath}/fastqFiles/pass/passed.fq --out_dir ${fullPath}/bcFastq/pass
 
-#cat ${fullPath}/fastqFiles/fail/* > ${fullPath}/fastqFiles/fail/failed.fq
-#deepbinner bin --classes ${fast5path}/classifications --reads ${fullPath}/fastqFiles/fail/failed.fq --out_dir ${fullPath}/bcFastq/fail
+cat ${fullPath}/fastqFiles/fail/* > ${fullPath}/fastqFiles/fail/failed.fq
+deepbinner bin --classes ${fullPath}/classifications --reads ${fullPath}/fastqFiles/fail/failed.fq --out_dir ${fullPath}/bcFastq/fail
 
 rm ${fullPath}/fastqFiles/pass/passed.fq
 rm ${fullPath}/fastqFiles/fail/failed.fq
